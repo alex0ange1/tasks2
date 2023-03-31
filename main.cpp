764dfbe9cc0ab4103cef73209ebc92a3a692a95b
Alexander Kudryashov <kudryashoff568@mail.ru>
2023-03-31 20:18:51 +0300

    Исправляем, чтобы посл-ть начиналась с (0, 1, ...)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 int fib(int n) {
-    if (n <= 2) {
+    if (n == 1) 
+        return 0;
+    else if (n == 2)
         return 1;
-    } else {
+    else {
         return fib(n-1) + fib(n-2);
     }
 }
