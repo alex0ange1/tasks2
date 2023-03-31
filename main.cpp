0a7cd201bb1e0402b293aa7bdb354b39f5dfb78a
Alexander Kudryashov <kudryashoff568@mail.ru>
2023-03-31 20:12:49 +0300

    Добавляем вычисление n-ого числа посл-ти чисел Фибоначчи (1, 1, ...)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+int fib(int n) {
+    if (n <= 2) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << fib(10) << std::endl;
     return 0;
+    
 }
